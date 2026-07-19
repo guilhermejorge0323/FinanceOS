7c507ef9d5add845ddb37e78339ce2b2c93d4ce0
Jorge <guilhermejorge272@gmail.com>
2026-07-19 02:40:04 -0300

    Subindo projeto

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactCompiler: true,
+
+  output: "export",
+
+  images: {
+    unoptimized: true,
+  },
+
+  basePath: process.env.NODE_ENV === "production" ? "/FinanceOS" : "",
 };
 
 export default nextConfig;
